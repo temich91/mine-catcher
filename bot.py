aaf8289f44463d8305b5199a6f97b1982b88d240
temich91 <temich-ivanoff@yandex.ru>
2025-02-23 01:38:03 +0500

    Field location fix (two bottom right corner variants); separated method for corner location

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ TEMPLATES_FILENAMES = [
 ]
 
 PHASH_THRESHOLD = 10
+IMAGE_LOCATION_CONFIDENCE = 0.99
 
 CELL_SYMBOLS = {
     "unsolved": ".",
@@ -47,25 +48,25 @@ class Bot:
         self.get_field_image()
         self.initialize_field()
 
-    def find_field(self):
+    def get_corner_coords(self, image_name, x_shift, y_shift):
         try:
-            ul_corner = list(pag.locateOnScreen("templates/top_left_corner.png", confidence=0.99))
-            ul_corner = list(map(int, ul_corner))
-
-            ul_corner[0] += 20
-            ul_corner[1] += 26
-
-            br_corner = list(pag.locateOnScreen("templates/bottom_right_corner.png"))
-            br_corner = list(map(int, br_corner))
-
-            br_corner[0] += 13
-            br_corner[1] += -2
-
-            return ul_corner, br_corner
+            corner = list(pag.locateOnScreen(f"templates/{image_name}.png", confidence=IMAGE_LOCATION_CONFIDENCE))
+            corner = list(map(int, corner))
+            corner[0] += x_shift
+            corner[1] += y_shift
+            return corner[0], corner[1]
 
         except pag.ImageNotFoundException:
             print("Game was not found. Сheck if the field is on the main screen and if it is fully positioned.")
             return False, False
+
+    def find_field(self):
+        ul_corner = self.get_corner_coords("top_left_corner", 20, 26)
+        br_corner = self.get_corner_coords("bottom_right_corner", 9, -1) or \
+                    self.get_corner_coords("bottom_right_corner_unsolved", 9, -1)
+        pag.moveTo(br_corner[0], br_corner[1])
+
+        return ul_corner, br_corner
 
     def check_game_state(self):
         try:
@@ -92,32 +93,42 @@ class Bot:
 
     def get_field_image(self):
         ul, br = self.find_field()
-        if not ul:
+
+        if not (ul[0] and br[0]):
             return
 
         field_screenshot = pag.screenshot(region=(ul[0], ul[1], br[0] - ul[0], br[1] - ul[1])) #x, y, width, height
         self.field_image = np.asarray(field_screenshot)
 
     def initialize_field(self):
+        if self.field_image is None:
+            return
         height, width = self.field_image.shape[:2]
         self.width = (width + 3) // 24
         self.height = (height + 3) // 24
         self.field = [["?" for _ in range(self.width)] for __ in range(self.height)]
 
     def scan_field(self):
+        if self.field_image is None:
+            return
+
         if self.check_game_state():
             print("Game over.")
             return
 
-        for y in range(self.height):
-            for x in range(self.width):
+        for x in range(self.height):
+            for y in range(self.width):
                 cell = self.field_image[x * 24: x * 24 + 21,
                                         y * 24: y * 24 + 21]
+                cv2.imwrite(f"q/cell_{x}_{y}.png", cell)
                 for templ_path in TEMPLATES_FILENAMES:
                     symbol = self.get_cell_symbol(cell, templ_path)
                     if symbol:
                         self.field[x][y] = symbol
                         break
+
+    def play(self):
+        pass
 
 bot = Bot()
 bot.scan_field()
